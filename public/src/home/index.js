c3e52cca15307573c13837c24d78ffe9a8914f03
Steven Roy <stevenandrewroy@gmail.com>
2019-10-24 16:57:31 +0100

    Basic React layout

---
+++ public/src/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div style="text-align: center;">
+                <h1>Durham Union Society Presidential Election</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
--- public/src/index.js
+++ public/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Route, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from "./home";
+
+class App extends React.Component {
+    render() {
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={Home} />
+            </Switch>
+        </div>
+    )
+    }
+}
+
+export default App;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
