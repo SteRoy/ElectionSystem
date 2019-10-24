6f965621cbb88f11564fd89f582bdeea3498f8b1
Steven Roy <stevenandrewroy@gmail.com>
2019-10-24 18:10:08 +0100

    Functioning landing page with placeholder for email processing

--- public/src/home/index.js
+++ public/src/home/index.js
@@ -2,13 +2,35 @@ import React from 'react';
 
 class Home extends React.Component {
     constructor(props) {
-        super();
+        super(props);
         this.state = {
-            emailAddress: ""
+            emailAddress: "",
+            error: ""
         }
     }
 
+    updateEmail(event) {
+        this.setState({
+            emailAddress: [event.target.value]
+        });
+    }
+
+    validateEmail(email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
     submitEmail() {
+        if (!this.validateEmail(this.state.emailAddress)) {
+            this.setState({
+                error: "You must use a valid email address."
+            });
+        } else {
+            this.setState({
+                error: ""
+            });
+            alert(this.state.emailAddress);
+        }
         return false;
     }
 
@@ -17,12 +39,13 @@ class Home extends React.Component {
             <div className="container" style={{"textAlign": "center"}}>
                 <h1>Durham Union Society Presidential Election</h1>
                 <hr/>
+                {this.state.error ? <div className="alert alert-danger">{this.state.error}</div> : ""}
                 <h4>Already a member of the Durham Union Society? Enter your email address below to get voting!</h4>
-                <div className="row">
-                    <input type="text" name="email" id="email" placeholder="Your DUS Email Address"/>
-                    <button className="btn btn-primary" onClick={this.submitEmail.bind(this)}>Vote</button>
+                <div className="form-group">
+                    <input className="form-control" type="text" name="email" id="email" onChange={this.updateEmail.bind(this)} placeholder="Your DUS Email Address"/>
+                    <button className="btn btn-primary form-control" onClick={this.submitEmail.bind(this)}>Vote</button>
                 </div>
-                <h7>(If you're not a member yet then head on over to <a href={"https://dus.upmind.app/auth/register"}>our website!</a></h7>
+                <h7>(If you're not a member yet then head on over to <a href={"https://dus.upmind.app/auth/register"}>our website!)</a></h7>
             </div>
         )
     }
