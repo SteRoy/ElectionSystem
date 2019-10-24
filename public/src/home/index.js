1eb801030165bf0a2c688e75487db0176222ce65
Steven Roy <stevenandrewroy@gmail.com>
2019-10-24 17:45:13 +0100

    Landing page to direct people to voting.

--- public/src/home/index.js
+++ public/src/home/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            emailAddress: ""
+        }
+    }
     render() {
         return (
             <div className="container" style={{"textAlign": "center"}}>
                 <h1>Durham Union Society Presidential Election</h1>
+                <hr/>
+                <h4>Already a member of the Durham Union Society? Enter your email address below to get voting!</h4>
+                <div className="row">
+                    <div className="col-sm-8">
+                        <input type="text" name="email" id="email" placeholder="Your DUS Email Address"/>
+                    </div>
+                </div>
+                <h7>(If you're not a member yet then head on over to <a href={"https://dus.upmind.app/auth/register"}>our website!</a></h7>
             </div>
         )
     }
